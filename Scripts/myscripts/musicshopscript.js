273af314c1c636e0faf235206dacf3802a8d626f
sandro gagoshidze <49387424+sannyybg@users.noreply.github.com>
2020-07-17 12:04:52 +0400

    Add files via upload

---
+++ Scripts/myscripts/musicshopscript.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    $("ul img").mouseover(function () {
+        $(this).animate({ height: '+=10', width: '+=10' })
+    });
+});
+
+$(function () {
+    $("ul img").mouseout(function () {
+        $(this).animate({ height: '-=10', width: '-=10' })
+    });
+});
+
+$(function () {
+    $("#inputphoto").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $("#previewphoto").attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
